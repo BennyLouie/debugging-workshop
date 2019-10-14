1ac171ea540fa2cccbed5c05cd021e17ad1d6c13
BennyLouie <bennylouie1412@gmail.com>
2019-10-14 10:58:45 -0400

    debugged

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  let jokeList = document.getElementById('joke-list')
+  let username = document.getElementById('name-input').value
   let joke;
-
+  
   function fetchJoke(){
     fetch('https://icanhazdadjoke.com/', {
       headers: {
-        "Accept": "application/json"
+        'Accept': 'application/json'
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      let newJokeLi = document.createElement('li')
+      joke = jokeData.joke
+
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+
+    })
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    username = event.target["name-input"].value
 
-    if(username === "") return;
+    if(username === ""){
+      return 
+    }
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
   })
 })
